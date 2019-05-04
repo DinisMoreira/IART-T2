fda78e6848c6a153b2b303c38d83f3a737730449
LuAs Miguel Pedrosa de Moura Oliveira Henriques <up201604343@feup-b213d08.fe.up.pt>
2019-05-04 12:57:55 +0100

    Added Problem constants

--- src/elements/Problem.java
+++ src/elements/Problem.java
@@ -7,6 +7,10 @@ import java.util.Map;
 
 public class Problem{
     
+    final private int numberOfDays = 5;
+    final private int hoursPerDay = 9;
+    final private int timeSlots = numberOfDays * hoursPerDay;
+
     private List<Room> rooms;
     private List<Student> students;
     private List<Event> events;
