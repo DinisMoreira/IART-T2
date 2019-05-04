34599474485a6fdeaa1c812216f141671ee3f45c
Dinis <dinismoreira1997@gmail.com>
2019-05-04 13:59:36 +0100

    started get problem from File function

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Main{
-    
-    public static void main(String[] args){
+import elements.*;
+
+public class Main {
+
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        //System.out.println("What is the name of the problem file?");
+        // System.out.println("What is the name of the problem file?");
         String fileName;// = scn.nextLine();
 
         fileName = args[0];
+
+        File file = new File("../ProblemFiles/" + fileName + ".tim");
+
+        Problem prob = new Problem();
+
+        try {
+            prob.getProblemFromFile(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        }
 
         
 
--- src/elements/Problem.java
+++ src/elements/Problem.java
@@ -1,13 +1,15 @@
 package elements;
 
-
 import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.HashMap;
 
-public class Problem{
-    
+public class Problem {
+
     final private int numberOfDays = 5;
     final private int hoursPerDay = 9;
     final private int timeSlots = numberOfDays * hoursPerDay;
@@ -16,9 +18,8 @@ public class Problem{
     private List<Student> students;
     private List<Event> events;
 
-    private List<Map<Student, Event>> studentEvents; //List containg matches of Students and Events
-    private List<Map<Room, Event>> roomEvents; //List containg matches of Rooms and Events
-
+    private List<Map<Student, Event>> studentEvents; // List containg matches of Students and Events
+    private List<Map<Room, Event>> roomEvents; // List containg matches of Rooms and Events
 
     public Problem() {
         this.rooms = new ArrayList<>();
@@ -29,30 +30,100 @@ public class Problem{
         this.roomEvents = new ArrayList<>();
     }
 
+    // Getters and Setters
+    public int getNumberOfDays() {
+        return this.numberOfDays;
+    }
 
+    public int getHoursPerDay() {
+        return this.hoursPerDay;
+    }
+
+    public int getTimeSlots() {
+        return this.timeSlots;
+    }
+
+    public List<Room> getRooms() {
+        return this.rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public List<Student> getStudents() {
+        return this.students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Event> getEvents() {
+        return this.events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    public List<Map<Student, Event>> getStudentEvents() {
+        return this.studentEvents;
+    }
+
+    public void setStudentEvents(List<Map<Student, Event>> studentEvents) {
+        this.studentEvents = studentEvents;
+    }
+
+    public List<Map<Room, Event>> getRoomEvents() {
+        return this.roomEvents;
+    }
+
+    public void setRoomEvents(List<Map<Room, Event>> roomEvents) {
+        this.roomEvents = roomEvents;
+    }
+
+    // Functions
     public void addRoom(Room room) {
-        if(this.rooms.contains(room) == false)
+        if (this.rooms.contains(room) == false)
             this.rooms.add(room);
     }
 
     public void addStudent(Student student) {
-        if(this.students.contains(student) == false)
+        if (this.students.contains(student) == false)
             this.students.add(student);
     }
 
     public void addEvent(Event event) {
-        if(this.events.contains(event) == false)
+        if (this.events.contains(event) == false)
             this.events.add(event);
     }
 
     public void addStudentEventRelation(Student student, Event event) {
-        if(this.students.contains(student))
+        if (this.students.contains(student))
             return;
-        if(this.events.contains(event))
+        if (this.events.contains(event))
             return;
 
+    }
+
+    public void getProblemFromFile(File file) throws FileNotFoundException {
+        Scanner f = new Scanner(file);
+
+        int numEvents = f.nextInt();
+        int numRooms = f.nextInt();
+        int numFeatures = f.nextInt();
+        int numStudents = f.nextInt();
+
+        System.out.println(numEvents);
+        System.out.println(numRooms);
+        System.out.println(numFeatures);
+        System.out.println(numStudents);
 
 
+        for(int i = 0; i < numRooms; i++){
+            Room room = new Room(i, f.nextInt());
+        }
     }
     
 }
