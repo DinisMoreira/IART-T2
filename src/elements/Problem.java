71efcb84aa768801f68e34e2aff1e092e23cd821
LuAs Miguel Pedrosa de Moura Oliveira Henriques <up201604343@feup-b213d08.fe.up.pt>
2019-05-04 13:23:54 +0100

    Added constructor to Problem

--- src/elements/Problem.java
+++ src/elements/Problem.java
@@ -4,6 +4,7 @@ package elements;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
 
 public class Problem{
     
@@ -15,13 +16,43 @@ public class Problem{
     private List<Student> students;
     private List<Event> events;
 
-    private Map<Student, Event> studentEvents;
-    private Map<Room, Event> roomEvents;
+    private List<Map<Student, Event>> studentEvents; //List containg matches of Students and Events
+    private List<Map<Room, Event>> roomEvents; //List containg matches of Rooms and Events
 
-    //Constructor
 
     public Problem() {
+        this.rooms = new ArrayList<>();
+        this.students = new ArrayList<>();
+        this.events = new ArrayList<>();
+
+        this.studentEvents = new ArrayList<>();
+        this.roomEvents = new ArrayList<>();
     }
-    
+
+
+    public void addRoom(Room room) {
+        if(this.rooms.contains(room) == false)
+            this.rooms.add(room);
+    }
+
+    public void addStudent(Student student) {
+        if(this.students.contains(student) == false)
+            this.students.add(student);
+    }
+
+    public void addEvent(Event event) {
+        if(this.events.contains(event) == false)
+            this.events.add(event);
+    }
+
+    public void addStudentEventRelation(Student student, Event event) {
+        if(this.students.contains(student))
+            return;
+        if(this.events.contains(event))
+            return;
+
+
+
+    }
     
 }
