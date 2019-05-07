1e594f6225112ae5f3a3e754f64a1c809d8d85f6
MiguelHenriques <pedrosa.miguel@gmail.com>
2019-05-07 14:16:12 +0100

    Solution now outputs to file

--- src/algorithms/HillClimbing.java
+++ src/algorithms/HillClimbing.java
@@ -1,5 +1,9 @@
 package algorithms;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import elements.*;
 
 public class HillClimbing{
@@ -23,10 +27,24 @@ public class HillClimbing{
         sol.allocateEvent(sol.getEventList().get(1).getID(), 2, prob.getRooms().get(0));
 
         System.out.println();
-        System.out.println("Event 0 - Time Slot: " + sol.getEventList().get(0).getTimeSlot() + " / Room: " + sol.getEventList().get(0).getRoom().getID());
+//        System.out.println("Event 0 - Time Slot: " + sol.getEventList().get(0).getTimeSlot() + " / Room: " + sol.getEventList().get(0).getRoom().getID());
         System.out.println("Event 1 - Time Slot: " + sol.getEventList().get(1).getTimeSlot() + " / Room: " + sol.getEventList().get(1).getRoom().getID());
 
     }
 
+    public void outputSolutionToFile(String fileName) {
+        File file = new File(fileName);
+        PrintWriter printWriter;
+        try {
+            printWriter = new PrintWriter(file);
+        } catch(FileNotFoundException e) {return;}
+        for(Event e : this.prob.getEvents()) {
+            //Pad number with max of 4 spaces
+            printWriter.printf("%4d %4d\n",
+                e.getTimeSlot(),
+                ((e.getRoom() == null) ? -1 : e.getRoom().getID()));
+        }
+        printWriter.close();
+    }
 
 }
